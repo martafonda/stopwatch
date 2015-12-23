9a6114b530d20d915b78990288f6f93f53f73820
Marta Fonda <martafondapascual@gmail.com>
2015-12-23 13:18:21 +0100

    Main functionality for reset, pause and start chronometer done and displayed in app

--- stopwatch/ViewController.swift
+++ stopwatch/ViewController.swift
@@ -10,20 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var chronometerInfo: UILabel!
     var timer = NSTimer()
-    var count = 0
+    var count:Int = 0
     
-    func chronometer(){
-    
-        count++
-        print(count)
-        
+    func displayChronometer(count: Int){
+        chronometerInfo.text = String(count)
     }
+    
+    func startChronometer(){
+        count++
+        displayChronometer(count)
+    }
+    
+    @IBAction func pauseButton(sender: UIBarButtonItem) {
+        timer.invalidate()
+    }
+    
+    @IBAction func startButton(sender: UIBarButtonItem) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("startChronometer"), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func resetButton(sender: UIBarButtonItem) {
+        timer.fire()
+        count = 0
+        displayChronometer(count)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("chronometer"), userInfo: nil, repeats: true)
+        
         
     }
 
