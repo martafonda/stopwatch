47caade2d8ee66c76c7141655860ee58adf3215b
Marta Fonda <martafondapascual@gmail.com>
2015-12-23 12:22:27 +0100

    main functionality done for chronometer in stopwatch - displaying seconds on console

--- stopwatch/ViewController.swift
+++ stopwatch/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var count = 0
+    
+    func chronometer(){
+    
+        count++
+        print(count)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("chronometer"), userInfo: nil, repeats: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
